dd5c0352eaf2306fcb5e4a6dbc0379cf25e0cc1e
buildwmish <hamish@hmrsportsolutions.co>
2026-04-06 11:41:44 +1200

    Update astro.config.mjs

--- hmr-site/astro.config.mjs
+++ hmr-site/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://hmrsportsolutions.co'
+  site: 'https://hmrsportsolutions.co',
+  integrations: [tailwind()],
 });
